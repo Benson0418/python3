e5b968a3c44b03f71e423b870af2afab4a1d32bf
Benson0418 <97838905+Benson0418@users.noreply.github.com>
2023-10-07 01:15:33 +0800

    Create 70. Climbing Stairs dp解.py

---
+++ 70. Climbing Stairs dp解.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n==1:
+            return 1
+        if n==2:
+            return 2
+        dp=[0,1,2]+[0]*(n-2)
+        for i in range(3,n+1):
+            dp[i]=dp[i-1]+dp[i-2]
+        return dp[-1]
