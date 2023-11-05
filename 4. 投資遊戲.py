370034d3564ae781826de1d31c68522a6738a559
Benson0418 <97838905+Benson0418@users.noreply.github.com>
2023-11-06 00:43:42 +0800

    Update 4. 投資遊戲.py

--- 4. 投資遊戲.py
+++ 4. 投資遊戲.py
@@ -10,7 +10,7 @@ for i in range(1,n):
 for _ in range(k):
     dp_curr[0]=dp_prev[0]
     for j in range(1,n):
-        dp_curr[j]=max(dp_curr[j-1]+profit[j],dp_prev[j-1])
+        dp_curr[j]=max(dp_curr[j-1]+profit[j],dp_prev[j-1]) #max(不使用金牌,使用金牌)
     dp_curr,dp_prev=dp_prev,dp_curr
 
 print(max(dp_prev))
