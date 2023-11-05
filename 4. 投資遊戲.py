23a091059a735723f00ffa1575b6d75135c5c683
Benson0418 <97838905+Benson0418@users.noreply.github.com>
2023-11-06 00:39:58 +0800

    Create 4. 投資遊戲.py

---
+++ 4. 投資遊戲.py
@@ -0,0 +1,16 @@
+n,k=map(int,input().split())
+profit=[int(x) for x in input().split()]
+dp_prev=[0]*n
+dp_curr=[0]*n
+
+dp_prev[0]=max(0,profit[0])
+for i in range(1,n):
+    dp_prev[i]=max(0,dp_prev[i-1]+profit[i])
+
+for _ in range(k):
+    dp_curr[0]=dp_prev[0]
+    for j in range(1,n):
+        dp_curr[j]=max(dp_curr[j-1]+profit[j],dp_prev[j-1])
+    dp_curr,dp_prev=dp_prev,dp_curr
+
+print(max(dp_prev))
