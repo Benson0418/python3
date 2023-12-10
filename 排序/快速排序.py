d37a096ab54e0cad3e6a67702bd9fc169cc9a26d
Benson0418 <97838905+Benson0418@users.noreply.github.com>
2023-12-10 12:23:31 +0800

    Update 快速排序.py

--- 排序/快速排序.py
+++ 排序/快速排序.py
@@ -1,19 +1,22 @@
+import random
+
 def quick_sort(arr):
     if len(arr) <= 1:
         return arr
-
-    pivot = arr[0]
-    left = []
-    right = []
+    p=random.randint(0,len(arr)-1)
+    arr[0],arr[p]=arr[p],arr[0]
+    pivot=arr[0]
+    left=[]
+    right=[]
     for i in range(1, len(arr)):
-        if arr[i] < pivot:
+        if arr[i]<pivot:
             left.append(arr[i])
         else:
             right.append(arr[i])
-    return quick_sort(left) + [pivot] + quick_sort(right)
+    return quick_sort(left)+[pivot]+quick_sort(right)
 
 
-import random,time
+import time
 
 test=[random.randint(1,100000) for _ in range(10000)]
 a=time.time()
