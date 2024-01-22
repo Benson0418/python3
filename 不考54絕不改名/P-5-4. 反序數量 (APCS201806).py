6ef44697130c2aafcb8491401bba3e3b65304c8a
Benson0418 <97838905+Benson0418@users.noreply.github.com>
2024-01-22 19:25:13 +0800

    Update P-5-4. 反序數量 (APCS201806).py

--- 不考54絕不改名/P-5-4. 反序數量 (APCS201806).py
+++ 不考54絕不改名/P-5-4. 反序數量 (APCS201806).py
@@ -1,3 +1,5 @@
+
+# 解1:
 from bisect import bisect_left
 
 n=int(input())
@@ -14,4 +16,26 @@ def sol(L):
     for k in lL:
         res+=bisect_left(rL,k)
     return res
+print(sol(L))
+
+#解2:
+n=int(input())
+L=[int(x) for x in input().split()]
+
+def sol(L):
+    if len(L)<=1:
+        return 0
+    mid=len(L)//2
+    res=0
+    lL,rL=L[:mid],L[mid:]
+    res+=sol(lL)+sol(rL)
+    lL.sort()
+    rL.sort()
+    k=0
+    for i in lL:
+        while k<len(rL) and i>rL[k]:
+            k+=1
+        res+=k
+    return res
+
 print(sol(L))
